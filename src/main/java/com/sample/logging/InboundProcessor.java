2d802709848723dc5b8172444716520d10c8156a
Jian Feng <jianfeng@Feng-MB-Pro.local>
2018-03-20 20:16:55 +0900

    clean up

--- src/main/java/com/sample/logging/InboundProcessor.java
+++ src/main/java/com/sample/logging/InboundProcessor.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.sample.logging;
 
@@ -11,28 +11,29 @@ import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sample.reportincident.InputReportIncident;
-
 /**
  * @author jianfeng
  *
  */
 public class InboundProcessor implements Processor {
 
-	private static final Logger log = LoggerFactory.getLogger(InboundProcessor.class);
+	private static final Logger audit_log = LoggerFactory.getLogger("rest-request-audit");
 
 	
+	public void init() {
+		audit_log.info(InboundProcessor.class + " is initialized.");
+	}
+
+
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.apache.camel.Processor#process(org.apache.camel.Exchange)
 	 */
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		log.info("This is inboundProcessor");
-		InputReportIncident body = exchange.getIn().getBody(InputReportIncident.class);
-		log.info("exchange.in.body={}", body);
-
+		audit_log.info("This is inboundProcessor");
+		
 		Map<String, String> outBody = new HashMap<String, String>();
 		outBody.put("code", "Accepted");
 		exchange.getOut().setBody(outBody);
